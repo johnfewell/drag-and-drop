557dc686536a29ba3b1dca6ac10617f8560feffe
meq <fewell@gmail.com>
2018-09-13 14:26:47 -0400

    reordering works

--- src/task.js
+++ src/task.js
@@ -1,21 +1,29 @@
-import React, { Component } from 'react';
-import styled from 'styled-components'
+import React, { Component } from "react";
+import styled from "styled-components";
+import { Draggable } from "react-beautiful-dnd";
 
 const Container = styled.div`
   padding: 8px;
   border: 1px solid lightgrey;
   border-radius: 2px;
   margin-bottom: 8px;
+  background-color: white;
 `;
 
 class Task extends Component {
-    render() {
-        return (
-            <Container>
-                {this.props.task.name}
-            </Container>
-        );
-    }
+  render() {
+    return (
+      <Draggable draggableId={this.props.task.id} index={this.props.index}>
+        {(provided) => (
+            <Container
+                {...provided.draggableProps}
+                {...provided.dragHandleProps}
+                innerRef={provided.innerRef }
+            >{this.props.task.name}</Container>
+        )}
+      </Draggable>
+    );
+  }
 }
 
 export default Task;
